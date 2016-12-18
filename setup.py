d09f78d6b8034cf6981e0f450bad8b8f74530f34
Anas Ghrab <anas@vps323249.ovh.net>
2016-12-18 11:41:49 +0100

    Correct version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='music22',
-      version='0.0.3dev3',
+      version='0.0.3dev4',
       description='A tool for musicological analysis from audio files. For a symbolic analysis, you can use Music21 (http://web.mit.edu/music21/). Now it is focused on modal music analysis : Scale analysis, tonic detection',
       long_description=readme(),
       classifiers=[
